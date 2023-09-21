0052e6e52831d0fd043af9836220187e71c948f7
dlsimpao <rlsimpao@ucdavis.edu>
2023-09-21 11:33:54 -0700

    integrate faker simulated data

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+const { faker } = require('@faker-js/faker');
 var duckdb = require('duckdb');
 
-var db = new duckdb.Database(':memory:');
+const randomName = faker.person.fullName(); // Rowan Nikolaus
+const randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
 
-var con = db.connect();
+console.log(randomName)
+console.log(randomEmail)
 
-con.run('CREATE TABLE a (i INTEGER)');
-var stmt = con.prepare('INSERT INTO a VALUES (?)');
-for (var i = 0; i < 10; i++) {
-  stmt.run(i);
-}
-stmt.finalize();
-con.all('SELECT * FROM a', function(err, res) {
-  if (err) {
-    throw err;
-  }
-  console.log(res)
-});
+// var db = new duckdb.Database(':memory:');
+
+// var con = db.connect();
+
+// con.run('CREATE TABLE a (i INTEGER)');
+// var stmt = con.prepare('INSERT INTO a VALUES (?)');
+// for (var i = 0; i < 10; i++) {
+//   stmt.run(i);
+// }
+// stmt.finalize();
+// con.all('SELECT * FROM a', function(err, res) {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log(res)
+// });
